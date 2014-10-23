4f5584718e59972ac8e5f381d145962a674c4e8a
Jacob Bondley <hasharahha@gmail.com>
2014-10-23 10:17:35 -0400

    Update Save.js

--- Save.js
+++ Save.js
@@ -1,5 +1,5 @@
 localStorage.setItem("Veiwer", "1");
-if(localStorage.getItem("user") !=null){
+if(localStorage.getItem("user")==null){
 var user = window.prompt("Please leave your Nick so we Know what to call");
 localStorage.setItem("user", user);
 }
