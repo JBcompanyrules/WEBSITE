a957a4ee4866e809c2369b506510307aca9f3a93
Jacob Bondley <hasharahha@gmail.com>
2014-10-24 10:35:46 -0400

    update

--- Save.js
+++ Save.js
@@ -3,6 +3,10 @@ if(localStorage.getItem("user")==null){
 var user = window.prompt("Please leave your Nick so we Know what to call");
 localStorage.setItem("user", user);
 }
+else if(user=="HASHARAHHA14"){
+  unlock();
+  alert("Welcome HASHARAHHA14")
+}
 else{
 alert("Welcome, "+localStorage.getItem("user"));
 }
